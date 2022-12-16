ceb29253a3ad6af7c5c8ff26777f07fa49496d23
zemferonok <zemferonok@gmail.com>
2022-12-16 16:35:03 +0100

    Async Await with fs.promises.

--- fs_path_util.js
+++ fs_path_util.js
@@ -2,61 +2,60 @@ const fs = require('node:fs');
 const path = require('node:path');
 const util = require('node:util');  // CallBack >> Promise
 
+function baseCRUD() {
+    fs.mkdir('./Dir/Trash', {recursive: true}, (err) => {
+        if (err) return console.log(err);
+    })  // Create the Directory
 
-fs.mkdir('./Dir/Trash', {recursive: true}, (err) => {
-    if (err) return console.log(err);
-})
+    fs.readdir('./Dir', (err, data) => {
+        if (err) return console.log(err);
+        console.log(data);
+    })  // Read the Directory
 
-fs.readdir('./Dir', (err, data) => {
-    if (err) return console.log(err);
-    console.log(data);
-})
+    fs.readdir('./Dir', {withFileTypes: true}, (err, data) => {
+        if (err) return console.log(err);
+        for (const element of data) {
+            console.log(element.name, 'is File', element.isFile());
+        }
+    })  // Read the Directory with FileTypes method
 
-fs.readdir('./Dir', {withFileTypes: true}, (err, data) => {
-    if (err) return console.log(err);
-    for (const element of data) {
-        console.log(element.name, 'is File', element.isFile());
-    }
-})
-
-fs.rmdir('./Dir', {recursive: true}, (err) => {
-    if (err) console.log(err);
-});
+    fs.rmdir('./Dir', {recursive: true}, (err) => {
+        if (err) console.log(err);
+    });
 
 
+    const pathToFile = path.join(__dirname, 'Dir', 'data.txt');
 
-const pathToFile = path.join(__dirname, 'Dir', 'data.txt');
+    fs.writeFile(pathToFile, 'First string', (err) => {
+        if (err) console.log(err);
+    })  // Will create File if it not exitsts
 
-fs.writeFile(pathToFile, 'First string', (err) => {
-    if (err) console.log(err);
-})  // Will create File if it not exitsts
+    fs.appendFile(pathToFile, '\n' + 'Added string', (err) => {
+        if (err) console.log(err);
+    })  // Will create File if it not exitsts
 
-fs.appendFile(pathToFile, '\n' + 'Added string', (err) => {
-    if (err) console.log(err);
-})  // Will create File if it not exitsts
+    fs.readFile(pathToFile, (err, data) => {
+        if (err) return console.log(err);
+        console.log(data.toString());
+    })
 
-fs.readFile(pathToFile, (err, data) => {
-    if (err) return console.log(err);
-    console.log(data.toString());
-})
+    fs.truncate(pathToFile, (err) => {
+        if (err) return console.log(err);
+        console.log('File was cleared')
+    })
 
-fs.truncate(pathToFile, (err) => {
-    if (err) return console.log(err);
-    console.log('File was cleared')
-})
+    const newPathToFile = path.join(__dirname, 'Dir', 'Trash', 'badData.txt');
 
-const newPathToFile = path.join(__dirname, 'Dir', 'Trash', 'badData.txt');
+    fs.rename(pathToFile, newPathToFile, (err) => {
+        if (err) return console.log(err);
+        console.log('The file was moved and renamed');
+    })
 
-fs.rename(pathToFile, newPathToFile, (err) => {
-    if (err) return console.log(err);
-    console.log('The file was moved and renamed');
-})
-
-fs.unlink(newPathToFile, (err) => {
-    if (err) return console.log(err);
-    console.log('File was deleted');
-})
-
+    fs.unlink(newPathToFile, (err) => {
+        if (err) return console.log(err);
+        console.log('File was deleted');
+    })
+}
 
 
 // ASYNC та AWAIT працює лише з Promices.
@@ -64,38 +63,73 @@ fs.unlink(newPathToFile, (err) => {
 // Використовується для синхронізації
 // і очікування асинхронних функцій.
 
-const mkdir = util.promisify(fs.mkdir);
-const writeFile = util.promisify(fs.writeFile);
-const readdir = util.promisify(fs.readdir);
-const rmdir = util.promisify(fs.rmdir);
-const setTimeoutP = util.promisify(setTimeout);
+async function makeAsyncAwait_1 () {
+    const setTimeoutP = util.promisify(setTimeout);
 
-async function makeMagic() {
     await setTimeoutP(1000).then(() => console.log('Dir is making..'))
-    await setTimeoutP(3000).then(() => console.log('Dir is maked'))
+    await setTimeoutP(1000).then(() => console.log('Dir is maked'))
+
+    await fs.promises.mkdir('./Dir/Trash', {recursive: true})
+        .catch(error => console.log(err))
+
+    await setTimeoutP(1000).then(() => console.log('File is making..'))
+    await setTimeoutP(1000).then(() => console.log('File is maked'))
+
+    const pathToFile = path.join(__dirname, 'Dir', 'data.txt');
+    await fs.promises.writeFile(pathToFile, 'First string')
+        .catch(error => console.log(err))
+
+    await setTimeoutP(1000).then(() => console.log('Dir is reading..'))
+    await setTimeoutP(1000).then(() => console.log('Dir is read'))
+
+    await fs.promises.readdir('./Dir')
+        .then(data => console.log(data))
+        .catch(error => console.log(err))
+
+    await setTimeoutP(1000).then(() => console.log('All is delliting..'))
+    await setTimeoutP(1000).then(() => console.log('All is deleted'))
+
+    await fs.promises.rmdir('./Dir', {recursive: true})
+        .catch(error => console.log(err));
+}
+
+async function makeAsyncAwait_2 () {
+    const mkdir = util.promisify(fs.mkdir);
+    const writeFile = util.promisify(fs.writeFile);
+    const readdir = util.promisify(fs.readdir);
+    const rmdir = util.promisify(fs.rmdir);
+    const setTimeoutP = util.promisify(setTimeout);
+
+    await setTimeoutP(1000).then(() => console.log('Dir is making..'))
+    await setTimeoutP(1000).then(() => console.log('Dir is maked'))
 
     await mkdir('./Dir/Trash', {recursive: true})
         .catch(error => console.log(err))
 
-    await setTimeoutP(3000).then(() => console.log('File is making..'))
-    await setTimeoutP(3000).then(() => console.log('File is maked'))
+    await setTimeoutP(1000).then(() => console.log('File is making..'))
+    await setTimeoutP(1000).then(() => console.log('File is maked'))
 
     const pathToFile = path.join(__dirname, 'Dir', 'data.txt');
     await writeFile(pathToFile, 'First string')
         .catch(error => console.log(err))
 
-    await setTimeoutP(3000).then(() => console.log('Dir is reading..'))
-    await setTimeoutP(3000).then(() => console.log('Dir is read'))
+    await setTimeoutP(1000).then(() => console.log('Dir is reading..'))
+    await setTimeoutP(1000).then(() => console.log('Dir is read'))
 
     await readdir('./Dir')
         .then(data => console.log(data))
         .catch(error => console.log(err))
 
-    await setTimeoutP(3000).then(() => console.log('All is delliting..'))
-    await setTimeoutP(3000).then(() => console.log('All is deleted'))
+    await setTimeoutP(1000).then(() => console.log('All is delliting..'))
+    await setTimeoutP(1000).then(() => console.log('All is deleted'))
 
     await rmdir('./Dir', {recursive: true})
         .catch(error => console.log(err));
 }
 
-makeMagic()
+
+module.exports = {
+    baseCRUD,
+    makeAsyncAwait_1,
+    makeAsyncAwait_2
+}
