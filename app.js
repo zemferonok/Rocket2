2a924c8be8bb96bc6123087be3521f249a0b0d73
zemferonok <zemferonok@gmail.com>
2023-01-07 11:55:02 +0100

    I love Diana

--- app.js
+++ app.js
@@ -2,19 +2,31 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
-const apiRouter = require('./api/api.router.js');
-const {MONGO_URL, PORT} = require("./configs/variables");
+const router = require('./src/router.js');
+const {MONGO_URL, PORT} = require("./configs/config");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-mongoose.set('strictQuery', true);
-mongoose.connect(MONGO_URL);    // Create & Connect to DB
-mongoose.set('debug', true);    // Turn ON debug mode
+app.use('/', router);
+app.use('*', (req, res) => {
+    res.json('Route not found');
+});
 
-app.use('/', apiRouter);
+mongoose.set('debug', true);
 
-app.listen(PORT, () => {
-    console.log('Listen', PORT, 'port');
+app.listen(PORT, async () => {
+    try {
+        mongoose.set('strictQuery', true);
+        const connection = await mongoose.connect(MONGO_URL);
+
+        if (connection) {
+            console.log('DB connected', MONGO_URL);
+        }
+    } catch (err) {
+        if (err) console.log(err);
+        process.exit(1);
+    }
+    console.log(`PORT: ${PORT} is listening`);
 });
--- src/user/user.router.js
+++ src/user/user.router.js
@@ -4,6 +4,7 @@ const controller = require('./user.controller.js');
 
 userRouter.get('/', controller.getAllUsers);
 userRouter.post('/', controller.createUser);
+
 userRouter.get('/:userId', controller.getUserById);
 userRouter.put('/:userId', controller.updateUserById);
 userRouter.delete('/:userId', controller.deleteUserById);
